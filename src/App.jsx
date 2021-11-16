c8d65003f05ae50e3cc17e1466334f220d570af2
helmax-y <max_rad@ukr.net>
2021-11-17 01:47:04 +0200

    Readme update. HashRouter is used

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import GlobalStyle from './styles';
 import Feed from './components/Feed';
 import Profile from './components/Profile';
@@ -8,13 +8,13 @@ function App() {
     return (
         <>
             <GlobalStyle />
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     <Route path="/" element={<Feed />} />
                     <Route path="/:tiktuker" element={<Profile />} />
                     <Route path="*" element={<Navigate to="/" />} />
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </>
     );
 }
